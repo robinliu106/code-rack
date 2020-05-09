bd2a5724af3d7101033dac7b09fafc0710121411
Robin Liu <robinliu106@gmail.com>
2020-05-09 16:54:32 -0400

    updated webpack for prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,34 @@
 const path = require("path");
-module.exports = {
-    entry: "./src/app.js",
-    // entry: "./src/playground/redux-expensify.js",
-    // entry: "./src/playground/destructuring.js",
-    // entry: "./src/playground/hoc.js",
 
-    output: {
-        path: path.join(__dirname, "public"),
-        filename: "bundle.js",
-    },
-    module: {
-        rules: [
-            {
-                loader: "babel-loader", //run files through babel
-                test: /\.js$/, //files that end with .js
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.s?css$/,
-                use: ["style-loader", "css-loader", "sass-loader"],
-            },
-        ],
-    },
-    devtool: "cheap-module-eval-source-map", //source map
-    devServer: {
-        contentBase: path.join(__dirname, "public"),
-        historyApiFallback: true,
-    },
+module.exports = (env) => {
+    const isProduction = env === "production";
+    return {
+        entry: "./src/app.js",
+        // entry: "./src/playground/redux-expensify.js",
+        // entry: "./src/playground/destructuring.js",
+        // entry: "./src/playground/hoc.js",
+
+        output: {
+            path: path.join(__dirname, "public"),
+            filename: "bundle.js",
+        },
+        module: {
+            rules: [
+                {
+                    loader: "babel-loader", //run files through babel
+                    test: /\.js$/, //files that end with .js
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.s?css$/,
+                    use: ["style-loader", "css-loader", "sass-loader"],
+                },
+            ],
+        },
+        devtool: isProduction ? "source-map" : "cheap-module-eval-source-map", //source map
+        devServer: {
+            contentBase: path.join(__dirname, "public"),
+            historyApiFallback: true,
+        },
+    };
 };
