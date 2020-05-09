1e6bc1c5b64f1f2b083b4c2d7a25a036de768137
Robin Liu <robinliu106@gmail.com>
2020-05-09 19:39:26 -0400

    setup devDependencies and public/dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env) => {
         // entry: "./src/playground/hoc.js",
 
         output: {
-            path: path.join(__dirname, "public"),
+            path: path.join(__dirname, "public", "dist"),
             filename: "bundle.js",
         },
         module: {
@@ -45,6 +45,7 @@ module.exports = (env) => {
         devServer: {
             contentBase: path.join(__dirname, "public"),
             historyApiFallback: true,
+            publicPath: "/dist/",
         },
     };
 };
